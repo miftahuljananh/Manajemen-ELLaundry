3e83795ce51b9995904ff07306fb544edfde203d
Miftahul Jannah <miftahuljannah16305@gmail.com>
2025-12-11 23:41:13 +0700

    koneksi.php

--- owner/koneksi.php
+++ owner/koneksi.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 if ($_SESSION) {
-    if ($_SESSION['role'] == 'owner') {
+    if ($_SESSION['role'] == 'admin') {
     } else {
         header("location:../index.php");
     }
@@ -9,7 +9,7 @@ if ($_SESSION) {
     header('location:../index.php');
 }
 
-$conn = mysqli_connect("localhost", "root", "", "db_laundry");
+$conn = mysqli_connect("localhost", "ellaundry_db", "ellaundry_db", "ellaundry_db");
 
 if (mysqli_connect_error()) {
     echo "Koneksi ke database gagal : " . mysqli_connect_error();

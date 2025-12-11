aec846e6791de5605e1abd67260d561393db3826
Miftahul Jannah <miftahuljannah16305@gmail.com>
2025-12-11 23:39:51 +0700

    koneksi.php

--- admin/koneksi.php
+++ admin/koneksi.php
@@ -9,7 +9,7 @@ if ($_SESSION) {
     header('location:../index.php');
 }
 
-$conn = mysqli_connect("localhost", "root", "", "db_laundry");
+$conn = mysqli_connect("localhost", "ellaundry_db", "ellaundry_db", "ellaundry_db");
 
 if (mysqli_connect_error()) {
     echo "Koneksi ke database gagal : " . mysqli_connect_error();

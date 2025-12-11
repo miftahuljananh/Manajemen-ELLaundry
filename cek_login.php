08e1228bce27b010808aaeb371300b02d4f570bd
Miftahul Jannah <miftahuljannah16305@gmail.com>
2025-12-11 23:38:57 +0700

    cek_login.php

--- cek_login.php
+++ cek_login.php
@@ -1,35 +1,42 @@
 <?php
 session_start();
-$conn = mysqli_connect("localhost", "root", "", "db_laundry");
+$conn = mysqli_connect("localhost", "ellaundry_db", "ellaundry_db", "ellaundry_db");
 
-$username = $_POST['username'];
+if (!isset($_POST['username']) || !isset($_POST['password'])) {
+    header("location:index.php?message=Invalid access");
+    exit();
+}
+
+$username = mysqli_real_escape_string($conn, $_POST['username']);
 $password = md5($_POST['password']);
 $query = "SELECT * FROM user WHERE username = '$username' AND password = '$password'";
-$row = mysqli_query($conn, $query);
-$data = mysqli_fetch_assoc($row);
-$cek = mysqli_num_rows($row);
+$result = mysqli_query($conn, $query);
+$data = mysqli_fetch_assoc($result);
 
-if ($cek > 0) {
+if ($data) {
+
+    $_SESSION['username'] = $data['username'];
+    $_SESSION['user_id'] = $data['id_user'];
+    $_SESSION['outlet_id'] = $data['outlet_id'];
+    $_SESSION['role'] = $data['role'];
+
     if ($data['role'] == 'admin') {
-        $_SESSION['role'] = 'admin';
-        $_SESSION['username'] = $data['username'];
-        $_SESSION['user_id'] = $data['id_user'];
-        $_SESSION['outlet_id'] = $data['outlet_id'];
-        header('location:admin');
-    } else if ($data['role'] == 'kasir') {
-        $_SESSION['role'] = 'kasir';
-        $_SESSION['username'] = $data['username'];
-        $_SESSION['user_id'] = $data['id_user'];
-        $_SESSION['outlet_id'] = $data['outlet_id'];
-        header('location:kasir');
-    } else if ($data['role'] == 'owner') {
-        $_SESSION['role'] = 'owner';
-        $_SESSION['username'] = $data['username'];
-        $_SESSION['user_id'] = $data['id_user'];
-        $_SESSION['outlet_id'] = $data['outlet_id'];
-        header('location:owner');
+        header("location:admin");
+        exit();
     }
+
+    if ($data['role'] == 'kasir') {
+        header("location:kasir");
+        exit();
+    }
+
+    if ($data['role'] == 'owner') {
+        header("location:owner");
+        exit();
+    }
+
 } else {
-    $message = 'Username atau password salah!!!';
-    header('location:index.php?message=' . $message);
+    header("location:index.php?message=Username atau password salah!");
+    exit();
 }
+?>
